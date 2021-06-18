f015eaab28b6d4b7ac71f539133af606a6e9726d
andrewh1994 <andrewhallinan1994@gmail.com>
2021-06-18 13:16:38 +0100

    AH - commit to remove unnecessary messsage

--- db/check_if_username_already_taken.php
+++ db/check_if_username_already_taken.php
@@ -24,8 +24,6 @@ if($result = mysqli_query($link, $sql)){
         // Free result set
         mysqli_free_result($result);
 
-    } else{
-        echo "No records matching your query were found.";
     }
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
